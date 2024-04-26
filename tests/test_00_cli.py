7e99d7ea8bbf8974315513a930f7b87875eb36ad
DrowsyDev <vyperius117@gmail.com>
2024-04-25 20:50:58 -0400

    Reverted changes

--- tests/test_00_cli.py
+++ tests/test_00_cli.py
@@ -8,7 +8,7 @@ Tests related to the CLI program.
 def test_no_arguments():
     code, out = call_securenv("")
     assert code != 0
-    assert 'error: the following arguments ar required' in str(out)
+    assert 'error: the following arguments are required' in str(out)
 
 
 def test_no_env_file():
